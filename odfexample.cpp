c779614fb25aa710e277af8fe354ed6fc6f4b0aa
attodf <92391242+attodf@users.noreply.github.com>
2021-10-12 19:08:05 +0200

    Add files via upload

---
+++ odfexample.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "***********************************************" << std::endl;
+    std::cout << "This application was downloaded via an ODF macro from https://github.com/attodf/odf-test and executed automatically." << std::endl;
+    std::cout << "Application: I do nothing!" << std::endl;
+    std::cout << "***********************************************" << std::endl;
+    system("PAUSE");
+}
+
